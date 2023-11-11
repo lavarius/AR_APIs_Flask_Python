efbf71ad2ba1ff4346a52f8c74fa452101ec4520
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2023-11-10 18:36:52 -0800

    simple type hinting for resources and models

--- models/item.py
+++ models/item.py
@@ -1,5 +1,6 @@
 from db import db
 
+from typing import Dict, List
 
 class ItemModel(db.Model):
     __tablename__ = "items"
@@ -11,12 +12,12 @@ class ItemModel(db.Model):
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"))
     store = db.relationship("StoreModel", back_populates="items")
 
-    def __init__(self, name, price, store_id):
+    def __init__(self, name: str, price: float, store_id: int):
         self.name = name
         self.price = price
         self.store_id = store_id
 
-    def json(self):
+    def json(self) -> Dict:
         return {
             "id": self.id,
             "name": self.name,
@@ -25,17 +26,17 @@ class ItemModel(db.Model):
         }
 
     @classmethod
-    def find_by_name(cls, name):
+    def find_by_name(cls, name: str):
         return cls.query.filter_by(name=name).first()
 
     @classmethod
-    def find_all(cls):
+    def find_all(cls) -> List:
         return cls.query.all()
 
-    def save_to_db(self):
+    def save_to_db(self) -> None:
         db.session.add(self)
         db.session.commit()
 
-    def delete_from_db(self):
+    def delete_from_db(self) -> None:
         db.session.delete(self)
         db.session.commit()
