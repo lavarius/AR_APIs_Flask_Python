ba277eb2e808f36733f4c45b28f55fa90ca2db39
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2023-11-14 16:18:43 -0800

    marshmallow schema for user

--- resources/user.py
+++ resources/user.py
@@ -15,7 +15,6 @@ from schemas.user import UserSchema
 from blocklist import BLOCKLIST
 
 
-BLANK_ERROR = "'{}' cannot be blank."
 USER_ALREADY_EXISTS = "A user with that username already exists."
 CREATED_SUCCESSFULLY = "User created successfully."
 USER_NOT_FOUND = "User not found."
